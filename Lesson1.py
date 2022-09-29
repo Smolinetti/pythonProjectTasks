17c6c79d6962f9ba430e80c8d5871cfb74bd391a
Smolinetti <2407373@mail.ru>
2022-09-29 12:35:58 +0300

    Lesson1_task2

--- Lesson1.py
+++ Lesson1.py
@@ -1,8 +1,6 @@
-
 # Задача №1
 # Напишите программу, которая принимает на вход цифру,
 # обозначающую день недели, и проверяет, является ли этот день выходным.
-
 
 day = int(input('Введите день недели от 1 до 7: '))
 
@@ -12,6 +10,33 @@ elif 0 < day < 6:
     print('Это будний день !')
 else:
     print('Это вне диапазона дня недели !')
+
+
+# Задача №2
+# Напишите программу для. проверки истинности утверждения
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+def inputNumbers(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def checkPredicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = inputNumbers(3)
+
+if checkPredicate(statement) == True:
+    print("Утверждение истинно")
+else:
+    print("Утверждение ложно")
 
 
 
