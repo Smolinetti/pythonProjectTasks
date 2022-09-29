bb640323cfdf2c266461d2e4dd699f1357a972fa
Smolinetti <2407373@mail.ru>
2022-09-29 10:53:49 +0300

    Lesson1_task1

---
+++ main.py
@@ -0,0 +1,22 @@
+# Нахождение максимума
+
+list = [5, 17, 15, 3, -1]
+max = 0
+for i in range(5):
+    if list[i] > max:
+        max = list[i]
+
+print (max)
+
+
+# Нахождение минимума
+
+x = [16, 9, 0, -1, 12]
+min = x[0]
+for i in x:
+    if i < min:
+        min = i
+
+print(min)
+
+
