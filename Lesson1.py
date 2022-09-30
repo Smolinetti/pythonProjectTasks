2a3ce978d2cd8420bcda95ebc7569d6caa6086ef
Smolinetti <2407373@mail.ru>
2022-09-30 22:57:55 +0300

    Lesson1_All_tasks

--- Lesson1.py
+++ Lesson1.py
@@ -1,6 +1,11 @@
 # Задача №1
 # Напишите программу, которая принимает на вход цифру,
 # обозначающую день недели, и проверяет, является ли этот день выходным.
+#
+# Пример:
+# - 6 -> да
+# - 7 -> да
+# - 1 -> нет
 
 day = int(input('Введите день недели от 1 до 7: '))
 
@@ -39,5 +44,88 @@ else:
     print("Утверждение ложно")
 
 
+# Задача №3
+# Напишите программу, которая принимает на вход координаты точки (X и Y),
+# причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится
+# эта точка (или на какой оси она находится).
+#
+# Пример:
+# - x=34; y=-30 -> 4
+# - x=2; y=4-> 1
+# - x=-34; y=-30 -> 3
+
+def inputKoord(x):
+    a = [0] * x
+    for i in range(x):
+        is_OK = False
+        while not is_OK:
+            try:
+                number = float(input(f"Введите {i+1} координату: "))
+                a[i] = number
+                is_OK = True
+                if a[i] == 0:
+                    is_OK = False
+                    print("Координата не должно быть равна 0 ")
+            except ValueError:
+                print("Ты ошибся. Вводить надо числа!")
+    return a
 
 
+def checkCoordinates(xy):
+    text = 4
+    if xy[0] > 0 and xy[1] > 0:
+        text = 1
+    elif xy[0] < 0 and xy[1] > 0:
+        text = 2
+    elif xy[0] < 0 and xy[1] < 0:
+        text = 3
+    print(f"Точка находится в {text} четверти плоскости")
+
+
+koordinate = inputKoord(2)
+checkCoordinates(koordinate)
+
+
+# Напишите программу, которая принимает на вход координаты двух точек
+# и находит расстояние между ними в 2D пространстве.
+#
+# Пример: - A (3,6); B (2,1) -> 5,09 - A (7,-5); B (1,-1) -> 7,21
+
+
+def inputNumbers(x):
+    xy = ["X", "Y"]
+    a = []
+    for i in range(x):
+        is_OK = False
+        while not is_OK:
+            try:
+                number = int(input(f"Введите координату по {xy[i]}: "))
+                a.append(number)
+                is_OK = True
+            except ValueError:
+                print("Ты ошибся. Вводить надо целые числа!")
+    return a
+
+
+def calculateLengthSegment(a, b):
+    lengthSegment = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5)
+    return lengthSegment
+
+
+print("Введите координаты точки А")
+pointA = inputNumbers(2)
+print("Введите координаты точки В")
+pointB = inputNumbers(2)
+
+print(f"Длина отрезка: {format(calculateLengthSegment(pointA, pointB), '.2f')}")
+
+
+# НЕОБЯЗАТЕЛЬНО
+# Напишите программу, которая принимает на вход число и проверяет,
+# кратно ли оно 5 и 10 или 15, но не 30.
+
+x = int(input("Введите число для проверки: "))
+if (x%5 == 0 and x%10 == 0 or x%15 == 0) and x%30 != 0:
+    print("Кратно")
+else:
+    print("Не кратно")
