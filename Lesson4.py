a0c553277603c50c5e5d0a6dbf52a8b930a09f1a
Smolinetti <2407373@mail.ru>
2022-10-13 15:23:20 +0300

    Lesson4 / All tasks

--- Lesson4.py
+++ Lesson4.py
@@ -81,3 +81,135 @@ print('Список неповторяющихся элементов:', final_l
 print('-' * 60)
 
 
+# Задача №4
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов
+# (значения от -100 до 100) многочлена и записать в файл многочлен степени k
+# k - максимальная степень многочлена, следующий степень на 1 меньше и так до ноля
+# Коэффициенты расставляет random, поэтому при коэффициенте 0 просто пропускаем данную
+# итерацию степени
+#
+# Пример:
+# k=2 -> 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10x² = 0
+# k=5 -> 3x⁵ + 5x⁴ - 6x³ - 3x = 0
+
+print("Задача №4 / Сформировать случайным образом список коэффициентов")
+print('-' * 10)
+
+import random
+
+
+def createDict():
+    equation = {}
+    degree = int(input('Введите максимальную степень многочлена: '))
+    for i in range(degree, -1, -1):
+        equation[i] = random.randint(-10, 10)
+    return equation
+
+
+def createEquation(equation: dict):
+    strEquation = ''
+    first = True
+
+    for k, v in equation.items():
+        if first:
+            first = False
+            if v > 0:
+                strEquation += f'{v}*x^{k}'
+            elif v < 0:
+                strEquation += f'- {abs(v)}*x^{k}'
+        else:
+            if v > 0:
+                strEquation += f' + {v}*x^{k}'
+            elif v < 0:
+                strEquation += f' - {abs(v)}*x^{k}'
+
+    return strEquation
+
+
+def printEquation(equation: str):
+    print(equation.replace('*x^1', 'x').replace('*x^0', '') + ' = 0')
+
+print(createEquation(createDict()))
+print('-' * 60)
+
+
+# Задача №5
+# Даны два файла, в каждом из которых находится запись многочлена.
+# Задача - сформировать файл, содержащий сумму многочленов.
+#
+# Пример двух заданных многочленов:
+# 23x⁹ - 16x⁸ + 3x⁷ + 15x⁴ - 2x³ + x² + 20 = 0
+# 17x⁹ + 15x⁸ - 8x⁷ + 15x⁶ - 10x⁴ + 7x³ - 13x¹ + 33 = 0
+#
+# Результат:
+# 40x⁹ - x⁸ -5x⁷ + 15x⁶ +5x⁴ + 5x³ + x² - 13x¹ + 53 = 0
+
+print("Задача №5 / Формирование файла, содержащий сумму многочленов")
+print('-' * 10)
+
+from random import randint as rI
+
+def createCoef():
+    coef = {}
+    degree = int(input("Введите максимальную степень: "))
+    for i in range(degree, -1, -1):
+        coef[i] = rI(-20,20)
+    return coef
+
+
+def createEquation(coefEquation: dict):
+    equation = ''
+    first = True
+
+    for i in coefEquation.items():
+        if first:
+            first = False
+            if i[1] < 0:
+                equation += ' -' + str(abs(i[1])) + 'x^' + str(i[0])
+            elif i[1] > 0:
+                equation += str(abs(i[1])) + 'x^' + str(i[0])
+        else:
+            if i[1] < 0:
+                equation += ' - ' + str(abs(i[1])) + 'x^' + str(i[0])
+            elif i[1] > 0:
+                equation += ' + ' + str(abs(i[1])) + 'x^' + str(i[0])
+
+    return equation + ' = 0'
+
+def parseEquation(equation: str):
+    equation = equation.replace(' + ', ' +').replace(' - ', ' -')
+    equation = equation.split()
+    equation = equation[:-2]
+
+    dictEquation = {}
+    for i in range(len(equation)):
+        equation[i] = equation[i].replace('+', '').split('x^')
+        dictEquation[int(equation[i][1])] = int(equation[i][0])
+    return dictEquation
+
+equation1 = createEquation(createCoef())
+equation2 = createEquation(createCoef())
+
+parEq1 = parseEquation(equation1)
+parEq2 = parseEquation(equation2)
+
+resultEquation ={}
+for i in range(max(len(parEq1), len(parEq2)), -1, -1):
+    first = parEq1.get(i)
+    second = parEq2.get(i)
+    if first != None or second != None:
+        resultEquation[i] = (first if first != None else 0) + (second if second != None else 0)
+
+def printEquation(equation: str):
+    print(equation.replace(" 1x", "x").replace("x^1", 'x').replace("x^0", ''))
+
+printEquation(createEquation(parEq1))
+printEquation(createEquation(parEq2))
+printEquation(createEquation(resultEquation))
+
+print('-' * 60)
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,4 @@
+import random
+
+list = [random.randint(1, 100) for i in range(10)]
+print(list)
