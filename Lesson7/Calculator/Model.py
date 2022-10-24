2bb917e3f191b8aad2cceea26495c35119e58b17
Smolinetti <2407373@mail.ru>
2022-10-24 09:35:53 +0300

    Lesson_7 / Calculator v1.0

---
+++ Lesson7/Calculator/Model.py
@@ -0,0 +1,37 @@
+first = 0
+second = 0
+result = 0
+
+listOperator = {'*': lambda x,y: int(x) * int(y),
+                '/': lambda x,y: int(x) / int(y),
+                '+': lambda x,y: int(x) + int(y),
+                '-': lambda x,y: int(x) - int(y)}
+
+
+def set_first(number: int):
+    global first
+    first = number
+
+def set_second(number: int):
+    global second
+    second = number
+
+def set_result(oper: str):
+
+    ### ДЕЛЕНИЕ НА НОЛЬ!!!!
+    global result
+    global second
+    result = listOperator.get(oper)(first, second)
+
+
+def get_first():
+    global first
+    return first
+
+def get_second():
+    global second
+    return second
+
+def get_result():
+    global result
+    return result
